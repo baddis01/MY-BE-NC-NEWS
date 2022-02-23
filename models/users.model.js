c5bc219d4c516449fcf72e40f65800e8d113136e
baddis01 <96215678+baddis01@users.noreply.github.com>
2022-02-23 10:22:43 +0000

    removed catch in users model

--- models/users.model.js
+++ models/users.model.js
@@ -1,12 +1,7 @@
 const db = require("../db/connection.js");
 
 exports.selectAllUsernames = () => {
-  return db
-    .query(`SELECT username FROM users`)
-    .then(({ rows }) => {
-      return rows;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  return db.query(`SELECT username FROM users`).then(({ rows }) => {
+    return rows;
+  });
 };
